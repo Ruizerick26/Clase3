12534964394b76310ea8aa88abdea03e49477fb8
Ruizerick26 <ruizerick2525@gmail.com>
2023-05-23 09:36:37 -0500

    "Primer comit"

---
+++ src/Genero.java
@@ -0,0 +1,77 @@
+public class Genero {
+    String tipo;
+    String ritnmo;
+    int anio;
+    String pais;
+    String artista;
+    String festival;
+
+    public Genero(String tipo, String ritnmo, int anio, String pais, String artista, String festival) {
+        this.tipo = tipo;
+        this.ritnmo = ritnmo;
+        this.anio = anio;
+        this.pais = pais;
+        this.artista = artista;
+        this.festival = festival;
+    }
+
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getRitnmo() {
+        return ritnmo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public String getFestival() {
+        return festival;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setRitnmo(String ritnmo) {
+        this.ritnmo = ritnmo;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public void setFestival(String festival) {
+        this.festival = festival;
+    }
+
+    public void imprimir_Datos(){
+        System.out.println(this.tipo);
+        System.out.println(this.ritnmo);
+        System.out.println(this.anio);
+        System.out.println(this.pais);
+        System.out.println(this.artista);
+        System.out.println(this.festival);
+    }
+}
+
+
---
+++ src/Principal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args){
+        Genero Rock= new Genero( "Rock","120bpn",1950,"Eu","Chuck Berry","Fesival de california" );
+        Genero Salsa=new Genero("Salsa","100bpn",1970,"Panama","Ruben Blades","Festival Internacional de salsa");
+        Genero Vallenato = new Genero("Vallenatos","80bpn", 2023,"Colombia","Binomio de oro","Tributo al vallenato");
+        System.out.println(Salsa.getAnio());
+        System.out.println(Rock.getAnio());
+        System.out.println(Vallenato.getAnio());
+        System.out.println(Salsa.getAnio()+Rock.getAnio()+Vallenato.getAnio());
+
+        Rock.imprimir_Datos();
+        System.out.printf("El genero %s posee un ritmo de %s y fue creado en el año %s y en el pais %s y uno de sus primeros artistas fue %s y toco en el %s");
+
+        Scanner valor = new Scanner(System.in);
+
+        System.out.println("Ingrese el año al que desea cambiar del genero Rock");
+        int aniotemp=valor.nextInt();
+        Rock.setAnio(aniotemp);
+        System.out.println("Ingrese el año al que desea cambiar del genero Salsa");
+        aniotemp=valor.nextInt();
+        Salsa.setAnio(aniotemp);
+        System.out.println("Ingrese el año al que desea cambiar del genero Vallenato");
+        aniotemp=valor.nextInt();
+        Vallenato.setAnio(aniotemp);
+
+        System.out.println("El nuevo año del genero rock es:"+Rock.getAnio());
+        System.out.println("El nuevo año del genero salsa es:"+Salsa.getAnio());
+        System.out.println("El nuevo año del genero vallenatoes:"+Vallenato.getAnio());
+
+
+    }
+}
